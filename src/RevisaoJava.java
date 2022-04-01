612398aabaa1dbf4014de4f8e844c99c32294390
MatheusViniciusDosSantos <matheus.vinicius77@hotmail.com>
2022-04-01 20:14:19 -0300

    Ajustes do codigo

--- src/RevisaoJava.java
+++ src/RevisaoJava.java
@@ -6,7 +6,12 @@ public class RevisaoJava {
 
 	public static void main(String[] args) {
 		String selecionarAcao = new String();
-		selecionarAcao = JOptionPane.showInputDialog("Escolha uma ação a ser executada: \r\n1 - Descobrir a fase da vida com base na idade.\r\n");
+		selecionarAcao = JOptionPane.showInputDialog("Escolha uma ação a ser executada: \r\n1 - Descobrir a fase da vida com base na idade.\r\n"
+				+ "2 - Inverter a ordem dor valores digitados.\r\n"
+				+ "3 - Apresentar 3 valores em ordem crescente.\r\n"
+				+ "4 - \r\n"
+				+ "5 - \r\n"
+				+ "6 - \r\n");
 		if (selecionarAcao != null) {
 			if (selecionarAcao.equals("1")) {
 				FaseDaVida.definirFasedaVida();
