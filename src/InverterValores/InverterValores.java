b3d246312763769458ae5591ca8ece2c77cad40a
MatheusViniciusDosSantos <matheus.vinicius77@hotmail.com>
2022-04-01 20:28:17 -0300

    Feita classe que inverte os valores de uma lista

---
+++ src/InverterValores/InverterValores.java
@@ -0,0 +1,24 @@
+package InverterValores;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+public class InverterValores {
+
+	public static void main(String[] args) {
+		String entrada = JOptionPane.showInputDialog("Digite números separados por vírgula para inverter sua ordem.");
+		String valores[] = entrada.split(",");
+		String resultado = new String();
+		for (int i = valores.length - 1; i >=0; i--) {
+			if (i == 0) {
+				resultado = resultado + valores[i];
+			} else {
+				resultado = resultado + valores[i] + ",";
+			}
+		}
+		System.out.println(resultado);
+	}
+
+}
