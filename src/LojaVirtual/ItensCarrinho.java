bd03e28e773df50919dcdef9d13ee94b4ce64360
Matheus Vinícius dos Santos <matheus.vinicius77@hotmail.com>
2022-04-04 12:42:27 -0300

    Feito método para adicionar produtos no carrinho

--- src/LojaVirtual/ItensCarrinho.java
+++ src/LojaVirtual/ItensCarrinho.java
@@ -5,4 +5,27 @@ import java.util.List;
 public class ItensCarrinho {
 	private List<Produto> produtos;
 	private Double valorCompra;
+	
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+	
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	
+	public Double getValorCompra() {
+		return valorCompra;
+	}
+	
+	public void setValorCompra(Double valorCompra) {
+		this.valorCompra = valorCompra;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "ItensCarrinho [produtos=" + produtos + ", valorCompra=" + valorCompra + "]";
+	}
 }
--- src/LojaVirtual/UsarLojaVirtual.java
+++ src/LojaVirtual/UsarLojaVirtual.java
@@ -1,6 +1,14 @@
 package LojaVirtual;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class UsarLojaVirtual {
+	private static int controlador;
+	private static List<Produto> listaProdutos;
+	private static List<Produto> listaProdutosCarrinho;
+	private static ItensCarrinho itensCarrinho;
+	private static Carrinho carrinho;
 
 	public static void main(String[] args) {
 		Produto alface = new Produto();
@@ -17,11 +25,71 @@ public class UsarLojaVirtual {
 		hamburguer.setQuantidadeEmEstoque(0);
 		hamburguer.setValor(5.0);
 		
+		macarrao.setId(3);
+		macarrao.setNome("Macarrão");
+		macarrao.setValor(10.0);
+		macarrao.setQuantidadeEmEstoque(2);
+		
+		listaProdutos.set(0, alface);
+		listaProdutos.set(1, hamburguer);
+		listaProdutos.set(2, macarrao);
+		itensCarrinho = new ItensCarrinho();
+		carrinho.setItensCarrinho(itensCarrinho);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		try {
+			while (controlador != 0) {
+				System.out.println("Digite o número correspondente para realizar uma ação na loja.\r\n"
+						+ "1 - Adicionar produtos no carrinho.\r\n"
+						+ "2 - Retirar produto do carrinho.\r\n");
+			}
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		
 
 	}
 	
 	public static void adicionarProdutosCarrinho() {
-		
+		int produtoEscolhido = 0;
+		System.out.println("Será moestrada uma lista de produtos em seguida digite o número do produto para adicionalo ao carrinho\r\n");
+		do {
+			System.out.println("1 - " + listaProdutos.get(0).toString() + "\r\n" 
+					+ "2 - " + listaProdutos.get(1).toString() + "\r\n" 
+					+ "3 - " + listaProdutos.get(2).toString() + "\r\n"
+					+ "0 - Não adicionar mais produtos" );
+			Scanner sc = new Scanner(System.in);
+			produtoEscolhido = sc.nextInt();
+			if (produtoEscolhido == 1) {
+				if (listaProdutos.get(0).getQuantidadeEmEstoque() > 0) {			
+					listaProdutosCarrinho.add(listaProdutos.get(0));
+					listaProdutos.get(0).setQuantidadeEmEstoque(listaProdutos.get(0).getQuantidadeEmEstoque() - 1);;
+					itensCarrinho.setProdutos(listaProdutosCarrinho);
+					sc.close();
+				} else {
+					System.out.println("Este produto está com falta de estoque, por favor escolha outro.");
+				}
+			} else if (produtoEscolhido == 2) {
+				if (listaProdutos.get(1).getQuantidadeEmEstoque() > 0) {
+					listaProdutosCarrinho.add(listaProdutos.get(1));
+					listaProdutos.get(1).setQuantidadeEmEstoque(listaProdutos.get(1).getQuantidadeEmEstoque() - 1);
+					itensCarrinho.setProdutos(listaProdutosCarrinho);
+					sc.close();
+				}
+			} else if (produtoEscolhido == 3) {
+				if (listaProdutos.get(2).getQuantidadeEmEstoque() > 0) {
+					listaProdutosCarrinho.add(listaProdutos.get(2));
+					listaProdutos.get(2).setQuantidadeEmEstoque(listaProdutos.get(2).getQuantidadeEmEstoque() - 1);
+					itensCarrinho.setProdutos(listaProdutosCarrinho);
+					sc.close();
+				}
+			} else if (produtoEscolhido == 0) {
+				System.out.println();
+				sc.close();
+			}
+		} while (produtoEscolhido != 0);
 	}
 	public static void removerProdutosCarrinho() {
 		
